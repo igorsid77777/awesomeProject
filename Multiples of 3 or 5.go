c99c4653f14ddd7b8db3cbfe1ad89373e6d06c94
igorsid77777 <igorsid77777@gmail.com>
2021-07-31 21:54:17 +0300

    + Multiples of 3 or 5

---
+++ Multiples of 3 or 5.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Multiple3And5(number int) int {
+	/*	i := 3
+		numForSum := make([]int, 0)
+		for i < number {
+
+			if i % 3 == 0 && i % 5 == 0 {
+				numForSum = append(numForSum, i)
+			}else if i % 3 == 0 {
+				numForSum = append(numForSum, i)
+			}else if i % 5 == 0{
+				numForSum = append(numForSum, i)
+			}
+			i++
+		}
+		k := 0
+		for j:= range numForSum{
+			k += numForSum[j]
+		}
+		return k*/
+
+	i := 3
+	numForSum := 0
+	for i < number {
+
+		if i%3 == 0 && i%5 == 0 {
+			numForSum += i
+		} else if i%3 == 0 {
+			numForSum += i
+		} else if i%5 == 0 {
+			numForSum += i
+		}
+		i++
+	}
+	return numForSum
+}
+
+func main() {
+	fmt.Println(Multiple3And5(15))
+}
